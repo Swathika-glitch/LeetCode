705d3c8feefddcc6df03616465f050b6b75e11b1
Swathika-glitch <swathika.gsk@gmail.com>
2025-10-20 19:52:43 +0530

    Time: 1 ms (74.53%), Space: 42.5 MB (96.36%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int c=0;
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("X++") || operations[i].equals("++X")){
+                c++;
+
+            }
+            else if (operations[i].equals("X--") || operations[i].equals("--X")){
+                c--;
+            }
+        }
+        return c;
+    }
+}
